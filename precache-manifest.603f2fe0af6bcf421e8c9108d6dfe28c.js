c76d86315a6c8303ccc682c5ce33611d767c0478
3v1lb1t <mike@shadowscape.io>
2025-04-18 10:45:44 -0600

    Updates

---
+++ precache-manifest.603f2fe0af6bcf421e8c9108d6dfe28c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33c78e1df836d8ebed14d53cd1b5569e",
+    "url": "/strength_app/index.html"
+  },
+  {
+    "revision": "026d75e8aa2af0a97f4e",
+    "url": "/strength_app/static/css/main.369f7745.chunk.css"
+  },
+  {
+    "revision": "88fee359538e19034561",
+    "url": "/strength_app/static/js/2.43d1106c.chunk.js"
+  },
+  {
+    "revision": "026d75e8aa2af0a97f4e",
+    "url": "/strength_app/static/js/main.f3b77f83.chunk.js"
+  },
+  {
+    "revision": "c9631d99ad27c076ae30",
+    "url": "/strength_app/static/js/runtime~main.9543bb26.js"
+  }
+]);
